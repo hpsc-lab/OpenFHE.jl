efee709a04cfdcd4f9b867d8a914ba3f02ee4094
Michael Schlottke-Lakemper <michael@sloede.com>
2023-12-28 07:04:39 +0100

    Expand example

--- examples/simple_real_numbers.jl
+++ examples/simple_real_numbers.jl
@@ -9,3 +9,15 @@ context = OpenFHE.Context(parameters)
 
 println("CKKS scheme is using ring dimension ", Int(OpenFHE.GetRingDimension(context)))
 println()
+
+keys = OpenFHE.KeyGen(context)
+
+EvalMultKeyGen(context, keys.private_key())
+
+EvalRotateKeyGen(context, keys.private_key(), [1, -2])
+
+x1 = [0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0]
+x2 = [5.0, 4.0, 3.0, 2.0, 1.0, 0.75, 0.5, 0.25]
+
+ptxt1 = MakeCKKSPackedPlaintext(context, x1)
+ptxt2 = MakeCKKSPackedPlaintext(context, x2)
