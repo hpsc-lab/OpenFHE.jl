152136f056a2662d256016ef0f175f9fb687caf0
Michael Schlottke-Lakemper <michael@sloede.com>
2023-12-29 06:17:59 +0100

    Add exports and convenience wrappers

--- examples/simple_real_numbers.jl
+++ examples/simple_real_numbers.jl
@@ -1,84 +1,84 @@
-using OpenFHE: OpenFHE
+using OpenFHE
 
 multDepth = 1
 scaleModSize = 50
 batchSize = 8
 
-parameters = OpenFHE.CCParams{OpenFHE.CryptoContextCKKSRNS}()
-OpenFHE.SetMultiplicativeDepth(parameters, multDepth)
-OpenFHE.SetScalingModSize(parameters, scaleModSize)
-OpenFHE.SetBatchSize(parameters, batchSize)
+parameters = CCParams{CryptoContextCKKSRNS}()
+SetMultiplicativeDepth(parameters, multDepth)
+SetScalingModSize(parameters, scaleModSize)
+SetBatchSize(parameters, batchSize)
 
-cc = OpenFHE.GenCryptoContext(parameters)
-OpenFHE.Enable(cc[], OpenFHE.PKE)
-OpenFHE.Enable(cc[], OpenFHE.KEYSWITCH)
-OpenFHE.Enable(cc[], OpenFHE.LEVELEDSHE)
-println("CKKS scheme is using ring dimension ", Int(OpenFHE.GetRingDimension(cc[])))
+cc = GenCryptoContext(parameters)
+Enable(cc, PKE)
+Enable(cc, KEYSWITCH)
+Enable(cc, LEVELEDSHE)
+println("CKKS scheme is using ring dimension ", Int(GetRingDimension(cc)))
 println()
 
-keys = OpenFHE.KeyGen(cc[])
+keys = KeyGen(cc)
 privkey = OpenFHE.private_key(keys)
 pubkey = OpenFHE.public_key(keys)
-OpenFHE.EvalMultKeyGen(cc[], privkey)
+EvalMultKeyGen(cc, privkey)
 
-OpenFHE.EvalRotateKeyGen(cc[], privkey, [1, -2])
+EvalRotateKeyGen(cc, privkey, [1, -2])
 
 x1 = [0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0]
 x2 = [5.0, 4.0, 3.0, 2.0, 1.0, 0.75, 0.5, 0.25]
 
-ptxt1 = OpenFHE.MakeCKKSPackedPlaintext(cc[], x1)
-ptxt2 = OpenFHE.MakeCKKSPackedPlaintext(cc[], x2)
+ptxt1 = MakeCKKSPackedPlaintext(cc, x1)
+ptxt2 = MakeCKKSPackedPlaintext(cc, x2)
 
 println("Input x1: ", ptxt1)
 println("Input x2: ", ptxt2)
 
-c1 = OpenFHE.Encrypt(cc[], pubkey, ptxt1)
-c2 = OpenFHE.Encrypt(cc[], pubkey, ptxt2)
+c1 = Encrypt(cc, pubkey, ptxt1)
+c2 = Encrypt(cc, pubkey, ptxt2)
 
-cAdd = OpenFHE.EvalAdd(cc[], c1, c2);
+cAdd = EvalAdd(cc, c1, c2);
 
-cSub = OpenFHE.EvalSub(cc[], c1, c2);
+cSub = EvalSub(cc, c1, c2);
 
-cScalar = OpenFHE.EvalMult(cc[], c1, 4.0);
+cScalar = EvalMult(cc, c1, 4.0);
 
-cMul = OpenFHE.EvalMult(cc[], c1, c2);
+cMul = EvalMult(cc, c1, c2);
 
-cRot1 = OpenFHE.EvalRotate(cc[], c1, 1);
-cRot2 = OpenFHE.EvalRotate(cc[], c1, -2);
+cRot1 = EvalRotate(cc, c1, 1);
+cRot2 = EvalRotate(cc, c1, -2);
 
-result = OpenFHE.Plaintext()
+result = Plaintext()
 
 println()
 println("Results of homomorphic computations: ")
 
-OpenFHE.Decrypt(cc[], privkey, c1, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, c1, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 print("x1 = ", result)
-println("Estimated precision in bits: ", round(OpenFHE.GetLogPrecision(result[]), sigdigits=8))
+println("Estimated precision in bits: ", round(GetLogPrecision(result), sigdigits=8))
 
-OpenFHE.Decrypt(cc[], privkey, cAdd, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cAdd, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 print("x1 + x2 = ", result)
-println("Estimated precision in bits: ", round(OpenFHE.GetLogPrecision(result[]), sigdigits=8))
+println("Estimated precision in bits: ", round(GetLogPrecision(result), sigdigits=8))
 
-OpenFHE.Decrypt(cc[], privkey, cSub, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cSub, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 println("x1 - x2 = ", result)
 
-OpenFHE.Decrypt(cc[], privkey, cScalar, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cScalar, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 println("4 * x1 = ", result)
 
-OpenFHE.Decrypt(cc[], privkey, cMul, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cMul, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 println("x1 * x2 = ", result)
 
-OpenFHE.Decrypt(cc[], privkey, cRot1, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cRot1, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 println()
 println("In rotations, very small outputs (~10^-10 here) correspond to 0's:")
 println("x1 rotate by 1 = ", result)
 
-OpenFHE.Decrypt(cc[], privkey, cRot2, OpenFHE.CxxPtr(result))
-OpenFHE.SetLength(result[], batchSize)
+Decrypt(cc, privkey, cRot2, OpenFHE.CxxPtr(result))
+SetLength(result, batchSize)
 println("x1 rotate by -2 = ", result)
--- src/OpenFHE.jl
+++ src/OpenFHE.jl
@@ -11,8 +11,47 @@ function __init__()
     @initcxx
 end
 
+
+# CCParams and its functions
+export CCParams, CryptoContextCKKSRNS, SetMultiplicativeDepth, SetScalingModSize,
+       SetBatchSize
+
+# CryptoContext and its functions
+export GenCryptoContext, Enable, GetRingDimension, KeyGen, EvalMultKeyGen, EvalRotateKeyGen,
+       MakeCKKSPackedPlaintext, Encrypt, EvalAdd, EvalSub, EvalMult, EvalRotate, Decrypt
+
+# Plaintext and its functions
+export Plaintext, SetLength, GetLogPrecision
+
+# PKESchemeFeature
+export PKESchemeFeature, PKE, KEYSWITCH, PRE, LEVELEDSHE, ADVANCEDSHE, MULTIPARTY, FHE,
+       SCHEMESWITCH
+
+
 function Base.show(io::IO, pt::CxxWrap.CxxWrapCore.SmartPointer{<:PlaintextImpl})
     print(io, _to_string_plaintext(pt))
+end
+
+# Convenience methods to avoid having to dereference smart pointers
+for (WrappedT, fun) in [
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :Enable,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :GetRingDimension,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :KeyGen,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalMultKeyGen,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalRotateKeyGen,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :MakeCKKSPackedPlaintext,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :Encrypt,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalAdd,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalSub,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalMult,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :EvalRotate,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.CryptoContextImpl{OpenFHE.DCRTPoly}}) => :Decrypt,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.PlaintextImpl}) => :SetLength,
+    :(CxxWrap.StdLib.SharedPtrAllocated{OpenFHE.PlaintextImpl}) => :GetLogPrecision,
+]
+    @eval function $fun(arg::$WrappedT, args...)
+        $fun(arg[], args...)
+    end
 end
 
 end # module OpenFHE
