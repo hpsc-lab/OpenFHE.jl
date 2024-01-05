d7b1ee6561949331210bd2063c6e11e3c4a09576
Michael Schlottke-Lakemper <michael@sloede.com>
2024-01-05 08:00:07 +0100

    Fix tests

--- test/test_auxiliary.jl
+++ test/test_auxiliary.jl
@@ -3,13 +3,13 @@ module TestAuxiliary
 using Test
 using OpenFHE
 
-@testset verbose=true showtiming=true "set_library!" begin
-    @test_nowarn OpenFHE.set_library!("wololo")
-    @test_nowarn OpenFHE.set_library!()
-end
-
 @testset verbose=true showtiming=true "library_path" begin
     @test_nowarn OpenFHE.library_path()
+end
+
+@testset verbose=true showtiming=true "set_library!" begin
+    @test_nowarn OpenFHE.set_library!(OpenFHE.library_path())
+    @test_nowarn OpenFHE.set_library!()
 end
 
 end # module
