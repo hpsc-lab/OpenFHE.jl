23854a61f0a02ad4b47ef22d94312a10cc049ebc
Michael Schlottke-Lakemper <michael@sloede.com>
2024-01-06 08:26:02 +0100

    Add docstrings for all enums

--- src/docs.jl
+++ src/docs.jl
@@ -40,6 +40,94 @@ Create an empty `Plaintext` object without any encoded data.
 Plaintext
 
 
+# Enums
+"""
+    PKESchemeFeature
+
+Enum type for selecting scheme features in a crypto context using [`Enable`](@ref).
+
+Instances are:
+* `PKE`
+* `KEYSWITCH`
+* `PRE`
+* `LEVELDSHE`
+* `ADVANCEDSHE`
+* `MULTIPARTY`
+* `FHE`
+* `SCHEMESWITCH`
+
+See also: [`Enable`](@ref)
+"""
+PKESchemeFeature, PKE, KEYSWITCH, PRE, LEVELEDSHE, ADVANCEDSHE, MULTIPARTY, FHE, SCHEMESWITCH
+
+"""
+    ScalingTechnique
+
+Enum type for selecting the scaling technique for ciphertext multiplication in a set of
+[`CCParams`](@ref) parameters using [`SetScalingTechnique`](@ref).
+
+Instances are:
+* `FIXEDMANUAL`
+* `FIXEDAUTO`
+* `FLEXIBLEAUTO`
+* `FLEXIBLEAUTOEXT`
+* `NORESCALE`
+* `INVALID_RS_TECHNIQUE`
+
+See also: [`SetScalingTechnique`](@ref)
+"""
+ScalingTechnique, FIXEDMANUAL, FIXEDAUTO, FLEXIBLEAUTO, FLEXIBLEAUTOEXT, NORESCALE,
+INVALID_RS_TECHNIQUE
+
+"""
+    SecretKeyDist
+
+Enum type for selecting the distribution from which the secret key is generated. To be
+used in a set of [`CCParams`](@ref) parameters using [`SetSecretKeyDist`](@ref).
+
+Instances are:
+* `GAUSSIAN`
+* `UNIFORM_TERNARY`
+* `SPARSE_TERNARY`
+
+See also: [`SetSecretKeyDist`](@ref)
+"""
+SecretKeyDist, GAUSSIAN, UNIFORM_TERNARY, SPARSE_TERNARY
+
+"""
+    DistributionType
+
+Enum type for specifing the secret key distribution according to the homomorphic encryption
+standard.
+
+Instances are:
+* `HEStd_uniform`
+* `HEStd_error`
+* `HEStd_ternary`
+
+"""
+DistributionType, HEStd_uniform, HEStd_error, HEStd_ternary
+
+"""
+   SecurityLevel
+Enum type for specifying the security level according to the homomorphic encryption standard
+in a set of [`CCParams`](@ref) parameters using [`SetSecurityLevel`](@ref).
+
+Instances are:
+* `HEStd_128_classic`
+* `HEStd_192_classic`
+* `HEStd_256_classic`
+* `HEStd_128_quantum`
+* `HEStd_192_quantum`
+* `HEStd_256_quantum`
+* `HEStd_NotSet`
+
+
+See also: [`SetSecurityLevel`](@ref)
+"""
+SecurityLevel, HEStd_128_classic, HEStd_192_classic, HEStd_256_classic, HEStd_128_quantum, HEStd_192_quantum, HEStd_256_quantum, HEStd_NotSet
+
+
 # Add convenience `show` methods to hide wrapping-induced ugliness
 for (T, str) in [
     :(CCParams{<:CryptoContextCKKSRNS}) => "CCParams{CryptoContextCKKSRNS}()",
